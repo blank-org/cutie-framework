6eba5cd70062cde430e94770e9748302d971f37d
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-02-14 03:58:50 +0530

    Added fadeLoading()

--- JS/AJAXLoad.js
+++ JS/AJAXLoad.js
@@ -2,16 +2,22 @@ function beginLoading() {
 	document.getElementById("wait_loader").classList.remove('hide');
 }
 
+function endLoading() {
+	document.getElementById("wait_loader").classList.add('hide');
+}
+
+function fadeLoading() {
+	setTimeout(function() {
+		endLoading();
+	}, 3000);
+}
+
 function errorLoading() {
 	document.getElementById("notification").classList.remove('hide');
 	clearTimeout(intrvl); // ensure single timer
 	intrvl = setInterval(function() {
 		document.getElementById("notification").classList.add('hide');
 	},3000);
-}
-
-function endLoading() {
-	document.getElementById("wait_loader").classList.add('hide');
 }
 
 function fbReload() {

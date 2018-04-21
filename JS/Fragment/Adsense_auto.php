75841e9057f7b1524703d8a4873e3ca0289950db
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-21 11:52:58 +0530

    Added adsense auto ads

---
+++ JS/Fragment/Adsense_auto.php
@@ -0,0 +1,7 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+	(adsbygoogle = window.adsbygoogle || []).push({
+		google_ad_client: "<?php echo $config['google_adsense_ad_client'] ?>",
+		enable_page_level_ads: true
+	});
+</script>

097f17f540488e00d5ed827b933b8f075301ed41
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-28 03:39:27 +0530

    negative count for set number of placeholders
    Always causes a placeholder's vertical space

--- HTML/Fragment/Item.php
+++ HTML/Fragment/Item.php
@@ -5,7 +5,10 @@
 
 	function placeholder($max_count, $nItems) {
 		if($max_count) {
-			$count = ($max_count - $nItems % $max_count) % $max_count;
+			if($max_count > 0)
+				$count = ($max_count - $nItems % $max_count) % $max_count;
+			else if($max_count < 0)
+				$count = abs($max_count);
 			while ($count--) {
 				?><div class='sidebar-nav-norm item_block-placeholder'></div><?php
 			}

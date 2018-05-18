2b8d0b6c8aebb35f772f45294fbd1909f174b938
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-05-19 04:54:40 +0530

    Formatting

--- JS/Fragment/Sentry_version.php
+++ JS/Fragment/Sentry_version.php
@@ -1,1 +1,1 @@
-src="https://cdn.ravenjs.com/<?php echo $config['sentry_version'] ?>/raven.min.js" crossorigin="anonymous"
+src='https://cdn.ravenjs.com/<?php echo $config['sentry_version'] ?>/raven.min.js' crossorigin='anonymous'

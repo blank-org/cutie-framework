8d2bb2e6a42cc6be1cd5cd47741569e7a02857df
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-05-02 20:48:14 +0530

    Hollow arrows to filled ones
    Display issue on android: glyph selection. At least approximate and manageable

--- HTML/Fragment/NavList.php
+++ HTML/Fragment/NavList.php
@@ -7,7 +7,7 @@
 	<a id='nav-menu-prev'
 	<?php if($prevId != '') { ?>
 		class='XURL item_block_container' href='<?php echo '/'.$prevId ?>' data-target='<?php echo $prevId ?>' data-title='<?php echo getComponentTitle($prevId) ?>' >
-		<img class='item_block_image item_block_image_visible' src='<?php echo getItemImageFileURL($prevId) ?>' alt='Navigation - previous page tile'><div class='item_block_text'><div class='arrow'>&#x25C1;</div><div><?php echo getComponentTitle($prevId) ?></div></div>
+		<img class='item_block_image item_block_image_visible' src='<?php echo getItemImageFileURL($prevId) ?>' alt='Navigation - previous page tile'><div class='item_block_text'><div class='arrow'>&#x25C4;</div><div><?php echo getComponentTitle($prevId) ?></div></div>
 	<?php }
 		else { ?>
 		class='item_block-disabled sidebar-nav-norm sidebar-nav-l-1' >
@@ -16,13 +16,13 @@
 	</a><a id='nav-menu-next'
 	<?php if($nextId != '') {?>
 		class='XURL item_block_container' href='<?php echo '/'.$nextId ?>' data-target='<?php echo $nextId ?>' data-title='<?php echo getComponentTitle($nextId) ?>' >
-		<img class='item_block_image item_block_image_visible' src='<?php echo getItemImageFileURL($nextId) ?>' alt='Navigation - next page tile'><div class='item_block_text'><div><?php echo getComponentTitle($nextId) ?></div><div class='arrow'>&#x25B7;</div></div>
+		<img class='item_block_image item_block_image_visible' src='<?php echo getItemImageFileURL($nextId) ?>' alt='Navigation - next page tile'><div class='item_block_text'><div><?php echo getComponentTitle($nextId) ?></div><div class='arrow'>&#x25BA;</div></div>
 	<?php }
 		else { ?>
 		class='item_block-disabled sidebar-nav-norm sidebar-nav-l-1' >
 		&nbsp;
 	<?php } ?>
 </a><a id='nav-menu-up' class='XURL item_block_container <?php if($parentId == 'root') echo 'center-arrow'?>' href='<?php echo getComponentURL($parentId) ?>' data-target='<?php echo $parentId ?>' data-title='<?php echo getComponentTitle($parentId) ?>'>
-		<img class='item_block_image <?php if($parentId == 'root') echo 'item_block_image_hidden'?>' src='<?php echo getItemImageFileURL($parentId) ?> ' alt='Navigation - up level tile'><div class='item_block_text'><div><?php if($parentId != 'root') echo getComponentTitle($parentId); ?></div><div class='arrow'>&#x25B3;</div></div>
+		<img class='item_block_image <?php if($parentId == 'root') echo 'item_block_image_hidden'?>' src='<?php echo getItemImageFileURL($parentId) ?> ' alt='Navigation - up level tile'><div class='item_block_text'><div><?php if($parentId != 'root') echo getComponentTitle($parentId); ?></div><div class='arrow'>&#x25B2;</div></div>
 	</a>
 </div>

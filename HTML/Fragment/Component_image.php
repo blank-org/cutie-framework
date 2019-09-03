6e0ead4358ba910ce94df46ba225f07666a726f8
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-09-04 01:52:31 +0530

    SVG max-height made optional, lazy loading

--- HTML/Fragment/Component_image.php
+++ HTML/Fragment/Component_image.php
@@ -7,10 +7,10 @@
 	else
 		list($width, $height, $type, $attr) = getimagesize($imageFileName);
 ?>
-<div class='content-image-container' style='max-height: <?php echo $height; ?>px'>
+<div class='content-image-container' <?php if(isset($max_height)) { ?> style='max-height: <?php echo $height; ?>px' <?php } ?>>
 	<div class="content-image <?php if(!empty($center)) echo 'center'; ?>" style="padding-bottom: <?php echo round($height/$width*100, 2)?>%">
 <?php if($ext == 'svg') { ?>
-				<object data='/<?php echo $id.'/'.$title.'.'.$ext ?>'></object>
+				<object data='/<?php echo $id.'/'.$title.'.'.$ext ?>' loading='lazy'></object>
 <?php } else { ?>
 				<img src='/<?php echo $id.'/'.$title.'.'.$ext ?>' loading='lazy' alt="<?php echo $alt ?>">
 <?php } ?>

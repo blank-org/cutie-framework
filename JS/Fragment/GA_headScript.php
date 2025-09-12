ca1c611d8a3dd5580b48a758c95f89d10002cc67
Ujjwal Singh <ujjwalsingh@outlook.com>
2025-09-13 04:14:14 +0530

    update to google tag manager

--- JS/Fragment/GA_headScript.php
+++ JS/Fragment/GA_headScript.php
@@ -1,11 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo htmlspecialchars($config['google_tag_id'], ENT_QUOTES, 'UTF-8'); ?>"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  ga('create', '<?php echo $config['google_analytics_id'] ?>', 'auto');
-	ga('require', 'linkid');
-  ga('send', 'pageview');
-
+  gtag('config', '<?php echo htmlspecialchars($config['google_tag_id'], ENT_QUOTES, 'UTF-8'); ?>');
 </script>

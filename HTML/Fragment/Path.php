082bf92ceaa1fb662160e932cf080528332a7199
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-08-04 09:22:47 +0530

    fix: language aware bread curmbs path

--- HTML/Fragment/Path.php
+++ HTML/Fragment/Path.php
@@ -4,11 +4,11 @@
 	else {
 		if(isComponentExternRoot('root')) {
 ?>
-	<a class='content-link' href='/'>&#8962; </a>
+	<a class='content-link' href='<?php echo getComponentURL('root') ?>'>&#8962; </a>
 <?php
 		} else {
 ?>
-	<a class='XURL content-link' href='/' data-target='root' data-title=''>&#8962; </a>
+	<a class='XURL content-link' href='<?php echo getComponentURL('root') ?>' data-target='root' data-title=''>&#8962; </a>
 <?php
 		}
 ?>
@@ -16,7 +16,7 @@
 <?php
 		foreach (getComponentPathStylized($id) as $key => $value) {
 ?>
-		<a class='XURL content-link' href='<?php echo '/'.$value[0] ?>' data-target='<?php echo $value[0] ?>' data-title='<?php echo $value[1] ?>'>
+		<a class='XURL content-link' href='<?php echo getComponentURLtrimmed($value[0]) ?>' data-target='<?php echo $value[0] ?>' data-title='<?php echo $value[1] ?>'>
 			<?php echo $value[1] ?>
 		</a>
 		<span class='path-separator'>\</span>
